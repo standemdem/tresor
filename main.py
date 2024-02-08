2c748dad9e0b657f1db7bf175ffa75302955b9ed
standemdem <stan.demontmarin@gmail.com>
2024-02-08 18:17:33 +0100

    added main() function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from functions import extract_tables, extract_matches, display_information
 
-if __name__ == "__main__":
+def main():
     pptx_file = "example FASEP.pptx"
     patterns = ["Montant du FASEP", 
                 "Date de signature de la convention", 
@@ -8,3 +8,6 @@ if __name__ == "__main__":
     tables = extract_tables(pptx_file)
     result = extract_matches(tables, patterns)
     display_information(result)
+
+if __name__ == "__main__":
+    main()
